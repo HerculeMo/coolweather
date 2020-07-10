31367567177de846e5e173a62214a7e0a577e9f4
Hanqiu Mo （莫汉秋） <Hanqiu.Mo@cerence.com>
2020-07-10 15:40:06 +0800

    show weather info

---
+++ app/src/main/java/com/cerence/hanqiumo/coolweather/gson/Basic.java
@@ -0,0 +1,18 @@
+package com.cerence.hanqiumo.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
--- app/src/main/java/com/cerence/hanqiumo/coolweather/util/Utility.java
+++ app/src/main/java/com/cerence/hanqiumo/coolweather/util/Utility.java
@@ -5,6 +5,9 @@ import android.text.TextUtils;
 import com.cerence.hanqiumo.coolweather.db.City;
 import com.cerence.hanqiumo.coolweather.db.Province;
 import com.cerence.hanqiumo.coolweather.db.County;
+import com.cerence.hanqiumo.coolweather.gson.Weather;
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -67,5 +70,17 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject=new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
